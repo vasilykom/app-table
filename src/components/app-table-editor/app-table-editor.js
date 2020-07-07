e70ea92119a54bb7f77887f9bdceebf89983fe86
Vasilijs <vasilykom@gmail.com>
2020-07-07 23:06:43 +0300

    add function

--- src/components/app-table-editor/app-table-editor.js
+++ src/components/app-table-editor/app-table-editor.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './app-table-editor.css';
 
-const AppTableEditor = () => {
-return (
+export default class AppTableEditor extends Component {
+
+  
+
+    render(){
+        return (
     
-    <div className="group-table">
-        <a name="top"></a>
-        <div className="board-row">
-            <input placeholder = "Name" type= "text" className="form-control"/>
-        </div>
-        <div className="board-row">
-            <input placeholder = "Surname" className="form-control"/>
-        </div>
-        <div className="board-row">
-            <input placeholder = "Age" className="form-control"/>
-        </div>
-        <div className="board-row">
-            <input placeholder = "City" className="form-control"/>  
-        </div>  
+            <div className="group-table">
+                <a name="top"></a>
+                <div className="board-row">
+                    <input placeholder = "Name" type= "text" className="form-control"/>
+                </div>
+                <div className="board-row">
+                    <input placeholder = "Surname" className="form-control"/>
+                </div>
+                <div className="board-row">
+                    <input placeholder = "Age" className="form-control"/>
+                </div>
+                <div className="board-row">
+                    <input placeholder = "City" className="form-control"/>  
+                </div>  
+        
+                <div className="board-btn">
+                    <button type="button" className="btn btn-primary btn-block"
+                    onClick={ () => this.props.onItemAdded('hello')}
+                    >ADD</button>
 
-        <div className="board-btn">
-            <button type="button" className="btn btn-primary btn-block">ADD</button>
-        </div>  
-    </div>
-
-    );
-};
-
-export default AppTableEditor;
+                </div>  
+            </div>
+        
+        );
+    }
+}
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,22 +5,23 @@ import AppTableList from '../app-table-list';
 
 export default class App extends Component {
 
+    startId=1000;
     state = {
          listData : [
-            {name: 'Vasily', surname: 'Komanovsky', age: '40', city: 'Vangazi', id: 1},
-            {name: 'Viktor', surname: 'Komanovsky', age: '40', city: 'Vangazi', id: 2},
-            {name: 'Vlad', surname: 'Komanovsky', age: '40', city: 'Vangazi', id: 3},
-            {name: 'Vasily', surname: 'Komanovsky', age: '40', city: 'Vangazi', id: 4},
-            {name: 'Viktor', surname: 'Komanovsky', age: '40', city: 'Vangazi', id: 5},
-            {name: 'Vlad', surname: 'Komanovsky', age: '40', city: 'Vangazi', id: 6},
-            {name: 'Vasily', surname: 'Komanovsky', age: '40', city: 'Vangazi', id: 7},
-            {name: 'Viktor', surname: 'Komanovsky', age: '40', city: 'Vangazi', id: 8},
-            {name: 'Vlad', surname: 'Komanovsky', age: '40', city: 'Vangazi', id: 9},
-            {name: 'Vasily', surname: 'Komanovsky', age: '40', city: 'Vangazi', id: 10},
-            {name: 'Viktor', surname: 'Komanovsky', age: '40', city: 'Vangazi', id: 11},
-            {name: 'Vlad', surname: 'Komanovsky', age: '40', city: 'Vangazi', id: 12}
+            this.createListData('vik','kvo',45,'city')
         ]
     }    
+
+    createListData(name, surname, age, city, id){
+         return{
+             name: name,
+             surname: surname,
+             age: age,
+             city: city,
+             id: this.startId++
+         }
+
+    }
 
     deleteItem = (id) => {
         this.setState (({ listData})=>{
@@ -35,11 +36,35 @@ export default class App extends Component {
         })
     }
 
+    addItem = (text) => {
+        
+        const newItem = {
+            name: text,
+            surname: text,
+            age: text,
+            city: text,
+            id: this.startId
+        }
+        this.setState (({ listData})=>{
+
+            const newArray = [
+                ...listData, newItem
+            ]
+
+            return {
+                listData: newArray
+            }
+        })
+
+    };
+
     render(){
         
         return(
             <div className="page">
-            <TableEditor/>
+            <TableEditor 
+            onItemAdded = {this.addItem}/>
+
             <AppTableList list={this.state.listData}
             onDeleted = { this.deleteItem}
             ></AppTableList>
