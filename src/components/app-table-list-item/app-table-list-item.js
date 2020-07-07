3b99e3995f3b48598d4f6a24943162393d130f6c
Vasilijs <vasilykom@gmail.com>
2020-07-07 16:44:36 +0300

    fixed href warnings

--- src/components/app-table-list-item/app-table-list-item.js
+++ src/components/app-table-list-item/app-table-list-item.js
@@ -15,8 +15,8 @@ const AppTableListItem = ({name,surname,age,city}) => {
 
                         <td className="td_edit">
                             <div className="td_edit_options">
-                                <a href="#" className="edit_row" alt="edit_btn">Edit</a> 
-                                <a href="#" className="delete_row">Delete</a> 
+                                <a href="/#" className="edit_row" alt="edit_btn">Edit</a> 
+                                <a href="/#" className="delete_row">Delete</a> 
                             </div>
                         </td>       
 
